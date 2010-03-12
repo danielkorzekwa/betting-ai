6cda65a9730879c8c2ecc5916f4444258838872b
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-03-12 21:38:51 +0000

    Issue: 1 Implement neural network that learns XOR operation

--- trunk/src/test/scala/dk/bettingai/examples/XorExampleTest.scala
+++ trunk/src/test/scala/dk/bettingai/examples/XorExampleTest.scala
@@ -9,34 +9,36 @@ import org.encog.neural.data.basic._
 import org.encog.neural.networks.training.propagation.resilient._
 import org.encog.neural.data._
 import java.util._
+import org.encog.neural.networks.logic._
 
-
+/**BasicNetwork - default settings
+ * 
+ * @author korzekwad
+ *
+ */
 class XorExampleTest {
 
-  private val xorInput = new Array[Array[Double]](4,2)
-  private val xorIdeal = new Array[Array[Double]](4,2)
+  private val xorInput = new Array[Array[Double]](4)
+  private val xorIdeal = new Array[Array[Double]](4)
   private val network  = new BasicNetwork() 
   
   @Before def setUp = {	  
-	xorInput(0)(0) = 0
-    xorInput(0)(1) = 0
-    xorInput(1)(0) = 1
-	xorInput(1)(1) = 0
-	xorInput(2)(0) = 0
-	xorInput(2)(1) = 1
-	xorInput(3)(0) = 1
-	xorInput(3)(1) = 1
+	
+	xorInput(0) = Array(0,0)
+    xorInput(1) = Array(1,0)
+	xorInput(2) = Array(0,1)
+	xorInput(3) = Array(1,1)
 			
-	xorIdeal(0)(0) = 0
-	xorIdeal(1)(0) = 1
-	xorIdeal(2)(0) = 1
-	xorIdeal(3)(0) = 0
+	xorIdeal(0) = Array(0)
+	xorIdeal(1) = Array(1)
+	xorIdeal(2) = Array(1)
+	xorIdeal(3) = Array(0)
 
 	network.addLayer(new BasicLayer(2))
 	network.addLayer(new BasicLayer(4))
 	network.addLayer(new BasicLayer(1))
     network.getStructure().finalizeStructure()
-    network.reset()
+    network.reset() 
   }
 	
   @Test def test() = {
@@ -57,7 +59,7 @@ class XorExampleTest {
   
   def computeAndAssert(dataPair:NeuralDataPair)= {
     val result:NeuralData = network.compute(dataPair.getIdeal)
-    println(dataPair.getInput().getData(0) + "," +dataPair.getInput().getData(0) + ", actual=" + + result.getData(0) + " ,ideal=" + dataPair.getIdeal().getData(0))
-    assertTrue("Error is bigger than 0.01",(dataPair.getIdeal().getData(0)-result.getData(0)).abs<=0.02)
+    println(dataPair.getInput().getData(0) + "," +dataPair.getInput().getData(1) + ", actual=" + + result.getData(0) + " ,ideal=" + dataPair.getIdeal().getData(0))
+    assertTrue("Error is bigger than 0.02",(dataPair.getIdeal().getData(0)-result.getData(0)).abs<=0.02)
   }
 }
