c7181ce22bfe7d3f29e8b5387fd664cc4a25fe2b
kamkor <kamkor@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-04-20 21:45:02 +0000

    Preparing project for configuration change (issue 25)

--- src/main/assembly/dist/start.bat
+++
@@ -1,3 +0,0 @@
-rem set DEBUG=-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n
-set JMX=-Dcom.sun.management.jmxremote.port=20000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
-java %JMX% %DEBUG% -Xmx512m -cp ./lib/betting-ai-${project.version}.jar dk.bettingai.marketsimulator.SimulatorApp %*
