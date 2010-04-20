fcba3c4170d6a4fe8be128843a955a00c95d1581
kamkor <kamkor@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-04-20 22:23:44 +0000

    Project configuration update (issue 25)

---
+++ trunk/market-simulator/src/test/scala/dk/bettingai/marketsimulator/SimulatorAppTest.scala
@@ -0,0 +1,13 @@
+package dk.bettingai.marketsimulator
+
+import org.junit._
+import Assert._
+import java.io._ 
+import java.util.zip._
+class SimulatorAppTest {
+
+  /** Check against exceptions only.
+  * 
+  */
+  @Test def test = SimulatorApp.main(Array())
+}
