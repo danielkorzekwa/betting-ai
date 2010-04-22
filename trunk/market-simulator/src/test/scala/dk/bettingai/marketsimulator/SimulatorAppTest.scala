1473fe7cd566796e429d6ad39ed18723b743701d
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-04-22 17:10:21 +0000

    Issue: 11 Market simulation for incorrect input parameters - DONE.

--- trunk/market-simulator/src/test/scala/dk/bettingai/marketsimulator/SimulatorAppTest.scala
+++ trunk/market-simulator/src/test/scala/dk/bettingai/marketsimulator/SimulatorAppTest.scala
@@ -6,8 +6,42 @@ import java.io._
 import java.util.zip._
 class SimulatorAppTest {
 
-  /** Check against exceptions only.
-  * 
-  */
-  @Test def test = SimulatorApp.main(Array())
+	/** Check against exceptions only.
+	 * 
+	 */
+	@Test def test = SimulatorApp.main(Array("marketData=marketData.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"))
+
+	@Test def testWrongNumberOfInputParameters() {
+		val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("incorrect parameters"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray), new String(consoleStream.toByteArray).contains("Wrong input parameters"))
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Usage"))
+	}
+	
+	@Test def testWrongMarketDataParameter() {
+		val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("marketDatasss=marketData.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Wrong input parameters"))
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Usage"))
+	}
+	
+	@Test def testWrongTraderImplParameter() {
+		val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("marketData=marketData.csv","traderImpldfdfd=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Wrong input parameters"))
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Usage"))
+	}
+	
+	@Test def testCorrectInputParameters() {
+		val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("marketData=marketData.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Simulation is started"))
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Simulation is finished in"))
+	}
 }
+
+
