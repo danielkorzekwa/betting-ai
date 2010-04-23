835055d7f18a7c848d4c5eabc539caa0c054ffab
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-04-23 13:46:56 +0000

    Issue: 5 Market simulation for not existing market data file - DONE
    Updating scala version to 2.8.0 RC1

--- trunk/market-simulator/src/main/scala/dk/bettingai/marketsimulator/SimulatorApp.scala
+++ trunk/market-simulator/src/main/scala/dk/bettingai/marketsimulator/SimulatorApp.scala
@@ -2,7 +2,7 @@
 package dk.bettingai.marketsimulator
 
 import java.io._
-
+import scala.io._
 /** Main class for the market simulator.
  * 
  * @author korzekwad
@@ -13,19 +13,20 @@ object SimulatorApp  {
 	def main(args:Array[String], console: PrintStream) {
 
 		printHeader(console)
-		
-		/**Wrong number of input arguments.*/
-		if(args.length!=2) {
-			printHelpMessage(console);
+
+		/**Wrong input parameters.*/
+		val argsMap:Map[String,String] = argsToMap(args)
+		if(!argsMap.contains("marketData") || !argsMap.contains("traderImpl")) {
+			printHelpMessage(console)
 			return
 		}
 
-		/**Wrong input arguments.*/
-		val argNames = args.map(arg => arg.split("=")(0))
-		if(!argNames.contains("marketData") || !argNames.contains("traderImpl")) {
-			printHelpMessage(console);
+		/**Market data file not found.*/
+		if(!new File(argsMap("marketData")).exists) {
+			console.println("Market data file not found: " + argsMap("marketData"))
 			return
 		}
+		val marketDataFile = Source.fromFile(new File(argsMap("marketData")))
 
 		console.println("""Simulation is started.Simulation progress: 1% 2% 3% 4% 5% 6%
 				.......................................................
@@ -39,16 +40,36 @@ object SimulatorApp  {
 
 	}
 
+
+	/**Map list of arguments to map, 
+	 * 
+	 * @param args
+	 * @return key - arg name, value - arg value, empty map is returned if can't parse input parameters.
+	 */
+	private def argsToMap(args:Array[String]):Map[String,String] = {
+			try {
+				if(args.length==2) {
+					Map(args.map(arg => (arg.split("=")(0),arg.split("=")(1))): _*)
+				}
+				else {
+					Map()
+				}
+			}
+			catch{
+			case e:Exception => Map()
+			}
+	}
+
 	private def printHeader(console:PrintStream) {
 		console.println("")
 		console.println("***********************************************************************************")
 		console.println("*Market Simulator Copyright 2010 Daniel Korzekwa(http://danmachine.com)           *")      
 		console.println("*Project homepage: http://code.google.com/p/betting-ai/                           *")
-	  console.println("*Licenced under Apache License 2.0(http://www.apache.org/licenses/LICENSE-2.0)    *")
-	  console.println("***********************************************************************************")
-	  console.println("")
+		console.println("*Licenced under Apache License 2.0(http://www.apache.org/licenses/LICENSE-2.0)    *")
+		console.println("***********************************************************************************")
+		console.println("")
 	}
-	
+
 	private def printHelpMessage(console:PrintStream) {
 		console.println("Wrong input parameters.\n")
 		console.println("Usage:")
