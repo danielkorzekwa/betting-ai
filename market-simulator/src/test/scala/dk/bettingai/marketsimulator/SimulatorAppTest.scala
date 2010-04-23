d3edfa1d0441c5520bdc0c8c0c6f363f78c9ab62
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-04-23 13:46:56 +0000

    Issue: 5 Market simulation for not existing market data file - DONE
    Updating scala version to 2.8.0 RC1

--- market-simulator/src/test/scala/dk/bettingai/marketsimulator/SimulatorAppTest.scala
+++ market-simulator/src/test/scala/dk/bettingai/marketsimulator/SimulatorAppTest.scala
@@ -35,9 +35,24 @@ class SimulatorAppTest {
 		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Usage"))
 	}
 	
+		@Test def testWrongFormatOfParameters() {
+		val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("marketDatamarketData.csv","traderImpldfdfddk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Wrong input parameters"))
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Usage"))
+	}
+	
+	@Test def testMotExistingMarketDataFile() {
+			val consoleStream = new ByteArrayOutputStream()
+		SimulatorApp.main(Array("marketData=/tmp/blabla/marketdata.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		
+		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Market data file not found: /tmp/blabla/marketdata.csv"))
+	}
+	
 	@Test def testCorrectInputParameters() {
 		val consoleStream = new ByteArrayOutputStream()
-		SimulatorApp.main(Array("marketData=marketData.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
+		SimulatorApp.main(Array("marketData=src/test/resources/marketDataEmpty.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl"),new PrintStream(consoleStream))
 		
 		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Simulation is started"))
 		assertTrue("Wrong output:\n" + new String(consoleStream.toByteArray),new String(consoleStream.toByteArray).contains("Simulation is finished in"))
