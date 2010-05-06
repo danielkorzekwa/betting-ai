98f300a735d2c2c9ff9702a55a3353d27d9c9143
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-05-06 18:21:46 +0000

    Issue 16: Create market - DONE

--- market-simulator/src/main/scala/dk/bettingai/marketsimulator/betex/Betex.scala
+++ market-simulator/src/main/scala/dk/bettingai/marketsimulator/betex/Betex.scala
@@ -20,4 +20,14 @@ trait Betex {
 	 * @param bet
 	*/
 	def placeBet(userId:Int,bet: Bet)
+	
+	/** Cancels a bet on a betting exchange.
+	 * 
+	 * @param userId Unique id of a user that cancels a bet.
+	 * @param betId Unique id of a bet to be cancelled.
+	*/
+	def cancelBet(userId: Int, betId:Long)
+	
+	/**Returns all markets that are not settled.*/
+	def getActiveMarkets(): List[Market]
 }
---
+++ market-simulator/src/main/scala/dk/bettingai/marketsimulator/betex/BetexImpl.scala
@@ -0,0 +1,35 @@
+package dk.bettingai.marketsimulator.betex
+
+import scala.collection.mutable.Map
+
+/** This class represents a betting exchange. It allows to create market, place bet, cancel bet, etc.
+ * @author korzekwad
+ *
+ */
+class BetexImpl extends Betex{
+
+	private val markets = Map[Long,Market]()
+	/**Creates market on a betting exchange.
+	 * 
+	 * @param market
+	 * 
+	 */
+	def createMarket(market:Market) = markets+= market.marketId -> market
+	
+	/** Places a bet on a betting exchange.
+	 * 
+	 * @param userId Unique id of a user that places a bet.
+	 * @param bet
+	*/
+	def placeBet(userId:Int,bet: Bet) = throw new UnsupportedOperationException("Not implemented")
+	
+	/** Cancels a bet on a betting exchange.
+	 * 
+	 * @param userId Unique id of a user that cancels a bet.
+	 * @param betId Unique id of a bet to be cancelled.
+	*/
+	def cancelBet(userId: Int, betId:Long) = throw new UnsupportedOperationException("Not implemented")
+	
+	/**Returns all markets that are not settled.*/
+	def getActiveMarkets():List[Market] = {markets.values.toList}
+}
