b6e287c24c8500e533dbe1379e6cb77331aa3f13
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-05-06 19:01:45 +0000

    Issue 17: Place bet (back/lay)
    Adding unit tests for negative scenarios.

--- market-simulator/src/main/scala/dk/bettingai/marketsimulator/betex/BetexImpl.scala
+++ market-simulator/src/main/scala/dk/bettingai/marketsimulator/betex/BetexImpl.scala
@@ -14,22 +14,26 @@ class BetexImpl extends Betex{
 	 * @param market
 	 * 
 	 */
-	def createMarket(market:Market) = markets+= market.marketId -> market
-	
+	def createMarket(market:Market) = {
+		require(!markets.contains(market.marketId),"Market already exist for marketId=" + market.marketId)
+		
+		markets+= market.marketId -> market
+	}
+
 	/** Places a bet on a betting exchange.
 	 * 
 	 * @param userId Unique id of a user that places a bet.
 	 * @param bet
-	*/
+	 */
 	def placeBet(userId:Int,bet: Bet) = throw new UnsupportedOperationException("Not implemented")
-	
+
 	/** Cancels a bet on a betting exchange.
 	 * 
 	 * @param userId Unique id of a user that cancels a bet.
 	 * @param betId Unique id of a bet to be cancelled.
-	*/
+	 */
 	def cancelBet(userId: Int, betId:Long) = throw new UnsupportedOperationException("Not implemented")
-	
+
 	/**Returns all markets that are not settled.*/
 	def getActiveMarkets():List[Market] = {markets.values.toList}
 }
