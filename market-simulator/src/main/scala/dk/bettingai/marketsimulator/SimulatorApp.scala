7ad14b08d8a32f0fd8a51051394cd330cbe072e3
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-06-11 10:52:21 +0000

    Issue 22:	First e2e test - Implementing SimulatorApp.

--- market-simulator/src/main/scala/dk/bettingai/marketsimulator/SimulatorApp.scala
+++ market-simulator/src/main/scala/dk/bettingai/marketsimulator/SimulatorApp.scala
@@ -3,6 +3,12 @@ package dk.bettingai.marketsimulator
 
 import java.io._
 import scala.io._
+import dk.bettingai.marketsimulator.betex.api._
+import dk.bettingai.marketsimulator.betex._
+import dk.bettingai.marketsimulator.marketevent._
+import dk.bettingai.marketsimulator.trader._
+import org.apache.commons.math.util.MathUtils._
+
 /** Main class for the market simulator.
  * 
  * @author korzekwad
@@ -26,7 +32,7 @@ object SimulatorApp  {
 		val marketDataFile = Source.fromFile(new File(argsMap("marketData")))
 
 		/**Load trader implementation class.*/
-		val traderImplClass = 
+		val traderImpl = 
 			try {
 				Class.forName(argsMap("traderImpl")).newInstance()
 			}
@@ -34,18 +40,48 @@ object SimulatorApp  {
 		case e:Exception => console.println("Can't load trader implementation class: " + argsMap("traderImpl") + ". Details: " + e); return
 		}
 
-		console.println("""Simulation is started.Simulation progress: 1% 2% 3% 4% 5% 6%
-				.......................................................
-				.................................................................................................................
-				..................................100%" +
-				Simulation is finished in 0 sec.
-				Expected profit report for trader com.dk.bettingai.trader.SimpleTraderImpl:
-				Man Utd vs Arsenal: Match Odds expProfit=3 expAggrProfit=3  mBets=1 uBets=1
-				-------------------------------------------------------------------------------------
-		TotalExpectedProfit=3 TotalMatchedBets=1 TotalUnmachedBets=0""")
+		console.print("Simulation is started.")
 
+		val betex = new Betex()
+		val marketEventProcessor = new MarketEventProcessorImpl(betex)
+		val traderUserId=100
+		val firstBetId=1000
+		val simulator = new Simulator(marketEventProcessor,traderImpl.asInstanceOf[ITrader],traderUserId,firstBetId,betex)
+
+		console.print(" Simulation progress:")
+
+		val time = System.currentTimeMillis
+		val marketDataFileSize = marketDataFile.size
+		var marketDataFileSizeRead=0
+		var currentProgress=0
+		for(marketEvent <- marketDataFile.reset.getLines()) {
+			marketDataFileSizeRead = marketDataFileSizeRead + marketEvent.size
+			currentProgress=(marketDataFileSizeRead*100)/marketDataFileSize
+			console.print(" " + currentProgress + "%")
+			simulator.process(marketEvent)
+			simulator.callTrader
+		}
+
+		/**Calculate analysis report.*/
+		val marketRiskReports = simulator.calculateRiskReport
+
+		if(currentProgress<100) console.print(" 100%")
+
+		console.print("\nSimulation is finished in %s seconds.".format((System.currentTimeMillis-time)/1000))
+		console.print("\n\nExpected profit report for trader " + argsMap("traderImpl") + ":")
+
+		var expAggrProfit=0d
+		for(r <- marketRiskReports) {
+			expAggrProfit = expAggrProfit	+ r.expectedProfit
+			console.print("\n%s: %s expProfit=%s expAggrProfit=%s mBets=%s uBets=%s".format(r.marketName,r.eventName,round(r.expectedProfit,2),round(expAggrProfit,2),r.matchedBetsNumber,r.unmatchedBetsNumber))
+		}
+		console.print("\n------------------------------------------------------------------------------------")
+
+		val totalExpectedProfit = marketRiskReports.foldLeft(0d)(_ + _.expectedProfit)
+		val aggrMatchedBets = marketRiskReports.foldLeft(0l)(_ + _.matchedBetsNumber)
+		val aggrUnmatchedBets = marketRiskReports.foldLeft(0l)(_ + _.unmatchedBetsNumber)
+		console.print("\nTotalExpectedProfit=%s TotalMatchedBets=%s TotalUnmachedBets=%s".format(round(totalExpectedProfit,2),aggrMatchedBets,aggrUnmatchedBets))
 	}
-
 
 	/**Map list of arguments to map, 
 	 * 
