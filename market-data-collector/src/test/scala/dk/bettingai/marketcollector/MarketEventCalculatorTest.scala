54e8d44b0e6b237d189714a0ccdc336591f6d681
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-07-02 12:56:29 +0000

    Issue 37:	Create MarketEventCalculator - Implementing MarketEventCalculator - in progress.

--- market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
+++ market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
@@ -39,27 +39,42 @@ class MarketEventCalculatorTest {
 	 * 
 	 * */
 	
-	@Test def testTradedVolumeAreTheSameNewMarketPricesAreAvailable {
-		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.87,45,0) :: new RunnerPrice(1.9,12,0) :: new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,23) :: new RunnerPrice(2.0,0,30):: Nil
-		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+	@Test def testTradedVolumeAreEmptyLayBetPlacementEventsAreGenerated {
+		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.87,45,0) :: new RunnerPrice(1.9,12,0) :: Nil
+		val newTradedVolume = Nil
 
-		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.87,45,0) :: new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,20) :: Nil
-		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.87,45,0) :: Nil
+		val previousTradedVolume = Nil
 
 		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
 		
 		val expectedEvents = 
 		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
 		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
+		  Nil
+		
+		assertEquals(2, marketEvents.size)
+		assertEquals(expectedEvents(0),marketEvents(0))
+		assertEquals(expectedEvents(1),marketEvents(1))
+	}
+	
+	@Test def testTradedVolumeAreEmptyBackBetPlacementEventsAreGenerated {
+		val newRunnerPrices = new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,23) :: new RunnerPrice(2.0,0,30):: Nil
+		val newTradedVolume = Nil
+
+		val previousRunnerPrices = new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,20) :: Nil
+		val previousTradedVolume = Nil
+
+		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
+		
+		val expectedEvents = 
 		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
 		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
 		  Nil
 		
-		assertEquals(4, marketEvents.size)
+		assertEquals(2, marketEvents.size)
 		assertEquals(expectedEvents(0),marketEvents(0))
 		assertEquals(expectedEvents(1),marketEvents(1))
-		assertEquals(expectedEvents(2),marketEvents(2))
-		assertEquals(expectedEvents(3),marketEvents(3))
 	}
 	
 @Test def testTradedVolumeAreTheSameNewMarketPriceWasRemoved {
@@ -78,8 +93,22 @@ class MarketEventCalculatorTest {
 		assertEquals(1, marketEvents.size)
 		assertEquals(expectedEvents(0),marketEvents(0))
 	}
+	
+	@Test def testTradedVolumeAreNotTheSameLayBetPlacementEventsAreGenerated{
+		val newRunnerPrices =  new RunnerPrice(1.85,2,0) :: Nil
+		val newTradedVolume = new PriceTradedVolume(1.85,13) :: Nil
 
+		val previousRunnerPrices = new RunnerPrice(1.85,5,0) :: Nil
+		val previousTradedVolume = new PriceTradedVolume(1.85,10) :: Nil
 
-
+		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
+		
+		val expectedEvents = 
+		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.85,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+		  Nil
+		
+		assertEquals(1, marketEvents.size)
+		assertEquals(expectedEvents(0),marketEvents(0))
+	}
 
 }
