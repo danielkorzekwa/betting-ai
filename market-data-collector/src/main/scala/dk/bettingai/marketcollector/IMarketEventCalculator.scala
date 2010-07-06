13b226ffadd09097635d1dc07b6f2815209392f4
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-07-06 10:54:18 +0000

    Issue 37:	Create MarketEventCalculator - Implementing MarketEventCalculator - in progress.

--- market-data-collector/src/main/scala/dk/bettingai/marketcollector/IMarketEventCalculator.scala
+++ market-data-collector/src/main/scala/dk/bettingai/marketcollector/IMarketEventCalculator.scala
@@ -2,21 +2,37 @@ package dk.bettingai.marketcollector
 
 import dk.bettingai.marketsimulator.betex.api.IMarket._
 
-/**This trait represents a function that calculates market events for the delta between previous and current state of the market runner.
+/**This trait represents a function that calculates market events for the delta between the previous and the current state of the market runner.
  * 
  * @author KorzekwaD
  *
  */
 trait IMarketEventCalculator {
 
-	/**Calculates market events for the delta between previous and current state of the market runner.
+	/**Calculates market events for the delta between the previous and the current state of the market runner.
 	 * 
 	 * @param userId The user Id that the bet placement events are calculated for.
 	 * @param marketId The market id that the bet placement events are calculated for. 
 	 * @param runnerId The market runner id that the bet placement events are calculated for. 
-	 * @param newMarketData Element 1 - marketPrices for the market runner, Element 2 - total traded volume for the market runner
-	 * @param previousMarketData Element 1 - marketPrices for the market runner, Element 2 - total traded volume for the market runner
+	 * @param runnerPricesDelta Delta between the new and the previous state of the runner prices.
+	 * @param runnerTradedVolumeDelta Delta between the new and the previous state of the runner traded volume.
 	 * @return List of market events in a json format (PLACE_BET, CANCEL_BET) for the market runner
 	 */
-	def calculate(userId:Long,marketId:Long,runnerId:Long)(newMarketData:Tuple2[List[IRunnerPrice],List[IPriceTradedVolume]],previousMarketData:Tuple2[List[IRunnerPrice],List[IPriceTradedVolume]]): List[String]
+	def calculateRunnerDelta(userId:Long,marketId:Long,runnerId:Long)(runnerPricesDelta:List[IRunnerPrice], runnerTradedVolumeDelta:List[IPriceTradedVolume]): List[String]
+
+	/**Calculates delta between the new and the previous state of the runner prices.
+	 * 
+	 * @param newRunnerPrices
+	 * @param previousRunnerPrices
+	 * @return Delta between the new and the previous state of the runner prices.
+	 */
+	def calculateRunnerPricesDelta(newRunnerPrices:List[IRunnerPrice],previousRunnerPrices:List[IRunnerPrice]):List[IRunnerPrice]
+	
+	/**Calculates delta between the new and the previous state of the runner traded volume.
+	 * 
+	 * @param newTradedVolumes
+	 * @param previousTradedVolumes
+	 * @return Delta between the new and the previous state of the runner traded volume.
+	 */
+	def calculateTradedVolumeDelta(newTradedVolumes:List[IPriceTradedVolume],previousTradedVolumes:List[IPriceTradedVolume]):List[IPriceTradedVolume]
 }
--- market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
+++ market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
@@ -6,108 +6,195 @@ import dk.bettingai.marketsimulator.betex.Market._
 
 class MarketEventCalculatorTest {
 
-	@Test def testBothRunnerStatesAreEmpty {
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(List()->List(),List()->List())
-		assertEquals(0, marketEvents.size)
+	/**
+	 * Tests scenarios for calculateRunnerPricesDelta.
+	 * */
+
+	@Test def testCalculateRunnerPricesDeltaBothRunnerPricesAreEmpty {
+		val runnerPricesDelta = MarketEventCalculator.calculateRunnerPricesDelta(List(),List())
+		assertEquals(0, runnerPricesDelta.size)
 	}
 
-	@Test def testBothRunnerStatesAreTheSame {
+	@Test def testCalculateRunnerPricesDeltaBothRunnerPricesAreTheSame {
 		val newRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
-		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
-
 		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+
+		val runnerPricesDelta = MarketEventCalculator.calculateRunnerPricesDelta(newRunnerPrices,previousRunnerPrices)
+		assertEquals(0, runnerPricesDelta.size)
+	}
+
+	@Test def testCalculateRunnerPricesDeltaNewRunnerPriceIsAvailable {
+		val newRunnerPrices = new RunnerPrice(1.85,15,7) :: new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+
+		val runnerPricesDelta = MarketEventCalculator.calculateRunnerPricesDelta(newRunnerPrices,previousRunnerPrices)
+		assertEquals(1, runnerPricesDelta.size)
+
+		assertEquals(1.85,runnerPricesDelta(0).price,0)
+		assertEquals(15,runnerPricesDelta(0).totalToBack,0)
+		assertEquals(7,runnerPricesDelta(0).totalToLay,0)
+	}
+
+	@Test def testCalculateRunnerPricesDeltaRunnerPricesAreUpdated {
+		val newRunnerPrices = new RunnerPrice(1.9,8,5) :: new RunnerPrice(1.95,6,12) :: Nil
+		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+
+		val runnerPricesDelta = MarketEventCalculator.calculateRunnerPricesDelta(newRunnerPrices,previousRunnerPrices)
+		assertEquals(2, runnerPricesDelta.size)
+
+		assertEquals(1.9,runnerPricesDelta(0).price,0)
+		assertEquals(-2,runnerPricesDelta(0).totalToBack,0)
+		assertEquals(5,runnerPricesDelta(0).totalToLay,0)
+
+		assertEquals(1.95,runnerPricesDelta(1).price,0)
+		assertEquals(6,runnerPricesDelta(1).totalToBack,0)
+		assertEquals(-8,runnerPricesDelta(1).totalToLay,0)
+	}
+
+	@Test def testCalculateRunnerPricesDeltaRunnerPriceIsNotAvailableAnymore {
+		val newRunnerPrices = new RunnerPrice(1.9,10,0) ::  Nil
+		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,5,20) :: Nil
+
+		val runnerPricesDelta = MarketEventCalculator.calculateRunnerPricesDelta(newRunnerPrices,previousRunnerPrices)
+		assertEquals(1, runnerPricesDelta.size)
+
+		assertEquals(1.95,runnerPricesDelta(0).price,0)
+		assertEquals(-5,runnerPricesDelta(0).totalToBack,0)
+		assertEquals(-20,runnerPricesDelta(0).totalToLay,0)
+	}
+
+	/**
+	 * Tests scenarios for calculateTradedVolumeDelta.
+	 * */
+
+	@Test def testCalculateRunnerTradedVolumesDeltaBothTradedVolumesAreEmpty {
+		val tradedVolumeDelta = MarketEventCalculator.calculateTradedVolumeDelta(List(),List())
+		assertEquals(0, tradedVolumeDelta.size)
+	}
+
+	@Test def testCalculateRunnerTradedVolumesDeltaBothRunnerTradedVolumesAreTheSame {
+		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
 		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
 
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
+		val tradedVolumeDelta = MarketEventCalculator.calculateTradedVolumeDelta(newTradedVolume,previousTradedVolume)
+		assertEquals(0, tradedVolumeDelta.size)
+	}
+
+	@Test def testCalculateRunnerTradedVolumesDeltaNewTradedVolumeIsAvailable {
+		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.85,150) :: new PriceTradedVolume(1.86,200) :: Nil
+		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+
+		val tradedVolumeDelta = MarketEventCalculator.calculateTradedVolumeDelta(newTradedVolume,previousTradedVolume)
+		assertEquals(1, tradedVolumeDelta.size)
+
+		assertEquals(1.85,tradedVolumeDelta(0).price,0)
+		assertEquals(150,tradedVolumeDelta(0).totalMatchedAmount,0)
+	}
+
+	@Test def testCalculateRunnerTradedVolumesDeltaRunnerTradedVolumesAreUpdated {
+		val newTradedVolume = new PriceTradedVolume(1.84,120) :: new PriceTradedVolume(1.86,170) :: Nil
+		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+
+		val tradedVolumeDelta = MarketEventCalculator.calculateTradedVolumeDelta(newTradedVolume,previousTradedVolume)
+		assertEquals(2, tradedVolumeDelta.size)
+
+		assertEquals(1.84,tradedVolumeDelta(0).price,0)
+		assertEquals(20,tradedVolumeDelta(0).totalMatchedAmount,0)
+
+		assertEquals(1.86,tradedVolumeDelta(1).price,0)
+		assertEquals(-30,tradedVolumeDelta(1).totalMatchedAmount,0)
+	}
+
+	@Test def testCalculateRunnerTradedVolumesDeltaRunnerTradedVolumeIsNotAvailableAnymore {
+		val newTradedVolume = new PriceTradedVolume(1.86,200) :: Nil
+		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+
+		val tradedVolumeDelta = MarketEventCalculator.calculateTradedVolumeDelta(newTradedVolume,previousTradedVolume)
+		assertEquals(1, tradedVolumeDelta.size)
+
+		assertEquals(1.84,tradedVolumeDelta(0).price,0)
+		assertEquals(-100,tradedVolumeDelta(0).totalMatchedAmount,0)
+	}
+
+
+	/**
+	 * Tests scenarios for calculateRunnerDelta.
+	 * */
+	@Test def testCalculateRunnerDeltaBothRunnerStatesAreEmpty {
+		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(List(),List())
 		assertEquals(0, marketEvents.size)
 	}
 
 	@Test(expected=classOf[IllegalArgumentException])  
-	def testTradedVolumeForThePreviousStateIsBiggerThanForTheNewState {
-		val newRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
-		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+	def testCalculateRunnerDeltaTradedVolumeForThePreviousStateIsBiggerThanForTheNewState {
+		val newRunnerPricesDelta = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+		val newTradedVolumesDelta = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,-20) :: Nil
 
-		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
-		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,201) :: Nil
-
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
+		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
 	}
 
-	/**
-	 * Tests for the most common scenario: If both previous and current market states are not the same 
-	 * then appropriate PLACE_BET/CANCEL_BET events are generated.
-	 * 
-	 * */
-	
-	@Test def testTradedVolumeAreEmptyLayBetPlacementEventsAreGenerated {
-		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.87,45,0) :: new RunnerPrice(1.9,12,0) :: Nil
-		val newTradedVolume = Nil
+	@Test def testCalculateRunnerDeltaTradedVolumeIsTheSameLayBetPlacementEventsAreGenerated {
+		val newRunnerPricesDelta = new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.9,2,0) :: Nil
+		val newTradedVolumesDelta = Nil
 
-		val previousRunnerPrices = new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.87,45,0) :: Nil
-		val previousTradedVolume = Nil
+		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
 
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
-		
 		val expectedEvents = 
-		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
-		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
-		  Nil
-		
-		assertEquals(2, marketEvents.size)
+			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
+				"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
+					Nil
+
+					assertEquals(2, marketEvents.size)
 		assertEquals(expectedEvents(0),marketEvents(0))
 		assertEquals(expectedEvents(1),marketEvents(1))
 	}
-	
-	@Test def testTradedVolumeAreEmptyBackBetPlacementEventsAreGenerated {
-		val newRunnerPrices = new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,23) :: new RunnerPrice(2.0,0,30):: Nil
-		val newTradedVolume = Nil
 
-		val previousRunnerPrices = new RunnerPrice(1.93,0,56) :: new RunnerPrice(1.95,0,20) :: Nil
-		val previousTradedVolume = Nil
+	@Test def testCalculateRunnerDeltaTradedVolumeIsTheSameBackBetPlacementEventsAreGenerated {
+		val newRunnerPricesDelta = new RunnerPrice(1.95,0,3) :: new RunnerPrice(2,0,30) :: Nil
+		val newTradedVolumesDelta = Nil
 
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
-		
+		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
+
 		val expectedEvents = 
-		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
-		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
-		  Nil
-		
-		assertEquals(2, marketEvents.size)
+			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+				"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+					Nil
+
+					assertEquals(2, marketEvents.size)
 		assertEquals(expectedEvents(0),marketEvents(0))
 		assertEquals(expectedEvents(1),marketEvents(1))
 	}
+	//
+	//	@Test def testCalculateRunnerDeltaTradedVolumeAreTheSameNewMarketPriceWasRemoved {
+	//		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.95,0,20) :: Nil
+	//		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+	//
+	//		val previousRunnerPrices = new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
+	//		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+	//
+	//		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
+	//
+	//		val expectedEvents = """
+	//			CANCEL_BET
+	//			""" :: Nil
+	//
+	//			assertEquals(1, marketEvents.size)
+	//		assertEquals(expectedEvents(0),marketEvents(0))
+	//	}
+	//
 	
-@Test def testTradedVolumeAreTheSameNewMarketPriceWasRemoved {
-		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.95,0,20) :: Nil
-		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+	@Test def testCalculateRunnerDeltaTradedVolumeIsNotTheSameLayBetPlacementEventsAreGenerated{
 
-		val previousRunnerPrices = new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
-		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
+		val newRunnerPricesDelta = new RunnerPrice(1.85,-3,0) :: Nil
+		val newTradedVolumesDelta = new PriceTradedVolume(1.85,3) :: Nil
 
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
-		
-		val expectedEvents = """
-				CANCEL_BET
-		""" :: Nil
-		
-		assertEquals(1, marketEvents.size)
-		assertEquals(expectedEvents(0),marketEvents(0))
-	}
-	
-	@Test def testTradedVolumeAreNotTheSameLayBetPlacementEventsAreGenerated{
-		val newRunnerPrices =  new RunnerPrice(1.85,2,0) :: Nil
-		val newTradedVolume = new PriceTradedVolume(1.85,13) :: Nil
+		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
 
-		val previousRunnerPrices = new RunnerPrice(1.85,5,0) :: Nil
-		val previousTradedVolume = new PriceTradedVolume(1.85,10) :: Nil
-
-		val marketEvents = MarketEventCalculator.calculate(123,10,1000)(newRunnerPrices->newTradedVolume,previousRunnerPrices->previousTradedVolume)
-		
 		val expectedEvents = 
-		  """{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.85,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
-		  Nil
-		
-		assertEquals(1, marketEvents.size)
+			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.85,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+				Nil
+
+				assertEquals(1, marketEvents.size)
 		assertEquals(expectedEvents(0),marketEvents(0))
 	}
 
