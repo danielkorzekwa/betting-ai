f6360f7fd170d9cb29076ea8a9352577e92b2971
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-07-08 16:58:27 +0000

    Issue 37:	Create MarketEventCalculator - Generating CANCEL_BETS event.

--- market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
+++ market-data-collector/src/test/scala/dk/bettingai/marketcollector/MarketEventCalculatorTest.scala
@@ -19,11 +19,11 @@ class MarketEventCalculatorTest {
 	@Test def testCalculateMarketEventsLayBetsAreGenerated {
 		val marketRunnerDelta = new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.9,2,0) :: Nil
 
-		val marketEvents = MarketEventCalculator.calculateMarketEvents(123,10,1000)(marketRunnerDelta)
+		val marketEvents = MarketEventCalculator.calculateMarketEvents(121,10,1000)(marketRunnerDelta)
 
 		val expectedEvents = 
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
-				"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
+			"""{"eventType":"PLACE_BET","userId":121,"betSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
+				"""{"eventType":"PLACE_BET","userId":121,"betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
 					Nil
 
 		assertEquals(2, marketEvents.size)
@@ -34,11 +34,11 @@ class MarketEventCalculatorTest {
 	@Test def testCalculateMarketEventsBackBetsAreGenerated {
 		val marketRunnerDelta = new RunnerPrice(1.95,0,3) :: new RunnerPrice(2,0,30) :: Nil
 
-		val marketEvents = MarketEventCalculator.calculateMarketEvents(123,10,1000)(marketRunnerDelta)
+		val marketEvents = MarketEventCalculator.calculateMarketEvents(122,10,1000)(marketRunnerDelta)
 
 		val expectedEvents = 
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
-				"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"PLACE_BET","userId":122,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+				"""{"eventType":"PLACE_BET","userId":122,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
 					Nil
 
 		assertEquals(2, marketEvents.size)
@@ -52,10 +52,10 @@ class MarketEventCalculatorTest {
 		val marketEvents = MarketEventCalculator.calculateMarketEvents(123,10,1000)(marketRunnerDelta)
 
 		val expectedEvents = 
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":7.0,"betPrice":1.95,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":5.0,"betPrice":2.0,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
-			"""{"eventType":"PLACE_BET","userId":123,"betId":100,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"PLACE_BET","userId":123,"betSize":7.0,"betPrice":1.95,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"PLACE_BET","userId":123,"betSize":5.0,"betPrice":2.0,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"PLACE_BET","userId":123,"betSize":3.0,"betPrice":1.95,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"PLACE_BET","userId":123,"betSize":30.0,"betPrice":2.0,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
 			Nil
 
 		assertEquals(4, marketEvents.size)
@@ -64,34 +64,34 @@ class MarketEventCalculatorTest {
 		assertEquals(expectedEvents(2),marketEvents(2))
 		assertEquals(expectedEvents(3),marketEvents(3))
 	}
+	
+@Test def testCalculateMarketEventsLayBetsAreCancelled {
+		val marketRunnerDelta = new RunnerPrice(1.85,-20,0) :: new RunnerPrice(1.9,-2,0) :: Nil
 
-	/**
-	 * Tests scenarios for calculateRunnerDelta - DeltaTradedVolumeIsTheSame - CANCEL BET.
-	 * */
+		val marketEvents = MarketEventCalculator.calculateMarketEvents(124,10,1000)(marketRunnerDelta)
 
-	/**
-	 * Tests scenarios for calculateRunnerDelta - DeltaTradedVolumeIsNotTheSame - CANCEL BET.
-	 * */
+		val expectedEvents = 
+			"""{"eventType":"CANCEL_BETS","userId":124,"betsSize":20.0,"betPrice":1.85,"betType":"LAY","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"CANCEL_BETS","userId":124,"betsSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":10,"runnerId":1000}""" :: 
+			Nil
 
-	//
-	//	@Test def testCalculateRunnerDeltaTradedVolumeAreTheSameNewMarketPriceWasRemoved {
-	//		val newRunnerPrices =  new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.95,0,20) :: Nil
-	//		val newTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
-	//
-	//		val previousRunnerPrices = new RunnerPrice(1.85,20,0) :: new RunnerPrice(1.9,10,0) :: new RunnerPrice(1.95,0,20) :: Nil
-	//		val previousTradedVolume = new PriceTradedVolume(1.84,100) :: new PriceTradedVolume(1.86,200) :: Nil
-	//
-	//		val marketEvents = MarketEventCalculator.calculateRunnerDelta(123,10,1000)(newRunnerPricesDelta,newTradedVolumesDelta)
-	//
-	//		val expectedEvents = """
-	//			CANCEL_BET
-	//			""" :: Nil
-	//
-	//			assertEquals(1, marketEvents.size)
-	//		assertEquals(expectedEvents(0),marketEvents(0))
-	//	}
-	//
+		assertEquals(2, marketEvents.size)
+		assertEquals(expectedEvents(0),marketEvents(0))
+		assertEquals(expectedEvents(1),marketEvents(1))
+	}
 
+@Test def testCalculateMarketEventsBackBetsAreCancelled {
+		val marketRunnerDelta = new RunnerPrice(1.85,0,-3) :: new RunnerPrice(1.9,0,-7) :: Nil
 
+		val marketEvents = MarketEventCalculator.calculateMarketEvents(124,10,1000)(marketRunnerDelta)
 
+		val expectedEvents = 
+			"""{"eventType":"CANCEL_BETS","userId":124,"betsSize":3.0,"betPrice":1.85,"betType":"BACK","marketId":10,"runnerId":1000}""" ::
+			"""{"eventType":"CANCEL_BETS","userId":124,"betsSize":7.0,"betPrice":1.9,"betType":"BACK","marketId":10,"runnerId":1000}""" :: 
+			Nil
+
+		assertEquals(2, marketEvents.size)
+		assertEquals(expectedEvents(0),marketEvents(0))
+		assertEquals(expectedEvents(1),marketEvents(1))
+	}
 }
