362fe29f8749a1abbf6ce44a03d3259422ee33d6
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-07-29 17:19:00 +0000

    Issue 36:  	 Create MarketEventProducer  - Adding junit test for MarketEventProducer

--- market-data-collector/src/test/scala/dk/bettingai/marketcollector/eventproducer/EventProducerTest.scala
+++ market-data-collector/src/test/scala/dk/bettingai/marketcollector/eventproducer/EventProducerTest.scala
@@ -2,71 +2,57 @@ package dk.bettingai.marketcollector.eventproducer
 
 import org.junit._
 import Assert._
-import java.text._
-import dk.bot.betfairservice._
-import java.util.Date
-import scala.collection.JavaConversions._
-import dk.bot.betfairservice.model._
-import org.joda.time._
-import dk.bettingai.marketcollector.marketservice._
 import dk.bettingai.marketsimulator.betex._
 import Market._
-import dk.bettingai.marketsimulator.marketevent._
-import org.apache.commons.math.util.MathUtils._
-import org.slf4j.LoggerFactory
-import dk.bettingai.marketcollector._
-import dk.bettingai.marketcollector.eventcalculator._
-import EventProducer._
 
 class EventProducerTest {
 
-	val log = LoggerFactory.getLogger(getClass)
+	val eventProducer:EventProducer = new EventProducer()
 
-	val betfairServiceFactoryBean = new dk.bot.betfairservice.DefaultBetFairServiceFactoryBean();
-	var betfairService:BetFairService = null
-	var marketService:MarketService = null
-	var eventProducer:EventProducer = null
+@Test
+def testProduce {
 
-	@Before
-	def setUp {
-		betfairServiceFactoryBean.setUser(System.getenv("bfUser"))
-		betfairServiceFactoryBean.setPassword(System.getenv("bfPassword"))
-		betfairServiceFactoryBean.setProductId(82)
-		val loginResponse = betfairServiceFactoryBean.login
-		betfairService = (betfairServiceFactoryBean.getObject.asInstanceOf[BetFairService])
-		marketService = new MarketService(betfairService)
-		eventProducer = new EventProducer()
-	}
-
-	@Test
-	def testCreateMarketEventIsReturned {
-		val now = new DateTime()
-		val marketIds = new MarketService(betfairService).getMarkets(now.minusMinutes(120).toDate, now.plusMinutes(660).toDate)		
-		val marketId = marketIds(1)
-
-		for(i <- 0 to 3) {
-			Thread.sleep(2000)
-			/**key - selectionId, value - runner prices + price traded volume*/
-			val marketRunnersMap:Map[Long,Tuple2[List[RunnerPrice],List[PriceTradedVolume]]] =  marketService.getMarketRunners(marketId)
-			try {
-			val events = eventProducer.produce(marketId,marketRunnersMap)
-			println("events:" + events.size)
-			val events2 = eventProducer.produce(marketId,marketRunnersMap)
-			assertEquals(0,events2.size)
-			} catch {
-				case e:EventProducerVerificationError => printDebug(e)
-			}
-		}
-	}
+	/**key - selectionId, value - runner prices + price traded volume*/
+	val runnerPrices = new RunnerPrice(2.0,23.53,0) :: new RunnerPrice(2.25,0,78.72) :: Nil 
+	val tradedVolume = new PriceTradedVolume(1.9,3.4) :: new PriceTradedVolume(2.3,8.23) :: Nil
+	val runnerPrices2 = new RunnerPrice(2.0,4.3,0) :: new RunnerPrice(2.1,100.02,0):: Nil 
+	val tradedVolume2 = new PriceTradedVolume(1.9,2.1) :: new PriceTradedVolume(2.0,2.5) :: Nil
+	val marketRunnersMap:Map[Long,Tuple2[List[RunnerPrice],List[PriceTradedVolume]]] =  Map((22,(runnerPrices,tradedVolume)),(23,(runnerPrices2,tradedVolume2)))
 	
-	private def printDebug(e:EventProducerVerificationError) {
-		println("EventProducerVerificationError: " + e.getLocalizedMessage)
-		println("Prev runner prices = " + e.prevRunnerData._1)
-		println("New runner prices =  " + e.newRunnerData._1)
-		println("Ver runner prices =  " + e.toVerifyRunnerData._1)
-		
-		println("Prev traded volume = " + e.prevRunnerData._2)
-		println("New traded volume =  " + e.newRunnerData._2)
-		println("Ver traded volume =  " + e.toVerifyRunnerData._2)
-	}
+	val events = eventProducer.produce(2,marketRunnersMap)
+	assertEquals(12,events.size)
+	assertEquals("""{"eventType":"PLACE_BET","betSize":8.0,"betPrice":2.3,"betType":"LAY","marketId":2,"runnerId":22}""",events(0))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":8.0,"betPrice":2.3,"betType":"BACK","marketId":2,"runnerId":22}""",events(1))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":3.0,"betPrice":1.9,"betType":"LAY","marketId":2,"runnerId":22}""",events(2))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":3.0,"betPrice":1.9,"betType":"BACK","marketId":2,"runnerId":22}""",events(3))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":23.0,"betPrice":2.0,"betType":"LAY","marketId":2,"runnerId":22}""",events(4))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":78.0,"betPrice":2.25,"betType":"BACK","marketId":2,"runnerId":22}""",events(5))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":2.0,"betPrice":2.0,"betType":"LAY","marketId":2,"runnerId":23}""",events(6))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":2.0,"betPrice":2.0,"betType":"BACK","marketId":2,"runnerId":23}""",events(7))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":2.0,"betPrice":1.9,"betType":"LAY","marketId":2,"runnerId":23}""",events(8))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":2.0,"betPrice":1.9,"betType":"BACK","marketId":2,"runnerId":23}""",events(9))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":4.0,"betPrice":2.0,"betType":"LAY","marketId":2,"runnerId":23}""",events(10))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":100.0,"betPrice":2.1,"betType":"LAY","marketId":2,"runnerId":23}""",events(11))
+
+	val nextRunnerPrices = new RunnerPrice(2.0,23.53,0) :: new RunnerPrice(2.25,0,88.72) :: Nil 
+	val nextTradedVolume = new PriceTradedVolume(1.9,13.4) :: new PriceTradedVolume(2.3,8.23) :: Nil
+	val nextRunnerPrices2 = new RunnerPrice(2.0,4.3,0) :: new RunnerPrice(2.1,100.02,0):: Nil 
+	val nextTradedVolume2 = new PriceTradedVolume(1.9,2.1) :: new PriceTradedVolume(2.0,2.5) :: Nil
+	val nextMarketRunnersMap:Map[Long,Tuple2[List[RunnerPrice],List[PriceTradedVolume]]] =  Map((22,(nextRunnerPrices,nextTradedVolume)),(23,(nextRunnerPrices2,nextTradedVolume2)))
+	
+	val events2 = eventProducer.produce(2,nextMarketRunnersMap)
+	assertEquals(5,events2.size)
+	assertEquals("""{"eventType":"CANCEL_BETS","betsSize":23.0,"betPrice":2.0,"betType":"LAY","marketId":2,"runnerId":22}""",events2(0))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":10.0,"betPrice":1.9,"betType":"LAY","marketId":2,"runnerId":22}""",events2(1))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":10.0,"betPrice":1.9,"betType":"BACK","marketId":2,"runnerId":22}""",events2(2))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":23.0,"betPrice":2.0,"betType":"LAY","marketId":2,"runnerId":22}""",events2(3))
+	assertEquals("""{"eventType":"PLACE_BET","betSize":10.0,"betPrice":2.25,"betType":"BACK","marketId":2,"runnerId":22}""",events2(4))
+	
+	val events3 = eventProducer.produce(2,nextMarketRunnersMap)
+	assertEquals(0,events3.size)
+	
+	val events4 = eventProducer.produce(3,nextMarketRunnersMap)
+	assertEquals(12,events4.size)
+}
+
 }
