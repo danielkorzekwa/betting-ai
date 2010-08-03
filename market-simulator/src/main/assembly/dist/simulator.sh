9314026da836e226d312375a020b5cd771cbd4ba
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-08-03 16:07:30 +0000

    Issue 39:	Create installation package for the betting ai - Launcher for market simulator has been moved to betting-ai-launcher.

--- market-simulator/src/main/assembly/dist/simulator.sh
+++
@@ -1,3 +0,0 @@
-export DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
-export JMX="-Dcom.sun.management.jmxremote.port=20000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-java $JMX $DEBUG -Xmx512m -cp ./lib/market-simulator-${project.version}.jar dk.bettingai.marketsimulator.SimulatorApp $*
