b1e51bf410a2447c8597887a36d2c09597756f3d
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-11-24 10:03:25 +0000

    Adding example trader that places bets based on ifwin/ilose delta.

--- trunk/trader-examples/src/main/scala/dk/bettingai/trader/ifwinlosedelta/IfwinlosedeltaTrader.scala
+++ trunk/trader-examples/src/main/scala/dk/bettingai/trader/ifwinlosedelta/IfwinlosedeltaTrader.scala
@@ -1,5 +1,6 @@
-package dk.bettingai.trader.prob
+package dk.bettingai.trader.ifwinlosedelta
 
+import scala.collection.JavaConversions._
 import dk.bettingai.marketsimulator.betex.api._
 import IMarket._
 import IBet.BetTypeEnum._
@@ -7,12 +8,12 @@ import dk.bettingai.marketsimulator.trader._
 import ITrader._
 import dk.bettingai.marketsimulator.betex._
 
-/** Creates time series chart with implied probability for all market runners.
+/** This trader places back and lay bets across all market runners based on ifWin-ifWose indicator.
  * 
  * @author korzekwad
- *  
+ *
  */
-class ProbabilityTrader extends ITrader{
+class IfwinlosedeltaTrader extends ITrader{
 
 	/**It is called once for every analysed market.
 	 * 
@@ -29,10 +30,15 @@ class ProbabilityTrader extends ITrader{
 
 	def execute(ctx: ITraderContext)= {
 
-		for(runnerId <- ctx.runners.map(_.runnerId)) {
-			val bestPrices = ctx.getBestPrices(runnerId)
-			val avgPrice = PriceUtil.avgPrice(bestPrices._1.price -> bestPrices._2 .price)
-			ctx.addChartValue(runnerId.toString, 1/avgPrice)
+		val risk = ctx.risk
+		for(runnerId <- ctx.runners.map(_.runnerId)) { 
+			val bestPrices = ctx.getBestPrices(runnerId)	
+
+			val deltaValue =risk.ifWin(runnerId) - risk.ifLose(runnerId)
+			if(deltaValue<=0 && !bestPrices._2.price.isNaN) ctx.fillBet(3,bestPrices._2.price,BACK,runnerId)
+			else if(!bestPrices._1.price.isNaN) ctx.fillBet(3,bestPrices._1.price,LAY,runnerId)
 		}
+		
+		ctx.addChartValue("ExpectedProfit",risk.marketExpectedProfit)
 	}
 }
