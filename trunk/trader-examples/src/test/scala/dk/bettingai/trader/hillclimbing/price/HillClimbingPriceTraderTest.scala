feda11d69410177756a00f262088e147580d1bf4
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2010-12-05 11:32:06 +0000

    Issue 63:  	 Trader optimisation for price variable adopting hill climbing algorithm

---
+++ trunk/trader-examples/src/test/scala/dk/bettingai/trader/hillclimbing/price/HillClimbingPriceTraderTest.scala
@@ -0,0 +1,31 @@
+package dk.bettingai.trader.hillclimbing.price
+
+import org.junit._
+import Assert._
+import dk.bettingai.marketsimulator.betex._
+import dk.bettingai.marketsimulator.marketevent._
+import dk.bettingai.marketsimulator._
+import java.io.File
+
+class HillClimbingPriceTraderTest {
+
+	val betex = new Betex()
+	val marketEventProcessor = new MarketEventProcessorImpl(betex)
+	val simulator = new Simulator(marketEventProcessor,betex)
+
+	val commission = 0.05
+	val traderUserId=100
+	val historicalDataUserId=1000
+
+	val marketDataDir = new File("./src/test/resources/one_hr_10mins_before_inplay")
+	val marketDataSources = Map (marketDataDir.listFiles.filter(_.getName.endsWith(".csv")).map(f => f.getName.split("\\.")(0).toLong -> f) : _*)
+	val trader = new HillClimbingPriceTrader()
+
+	@Test def test {
+		for(i <- 1 to 5) {
+			betex.clear()
+			simulator.runSimulation(marketDataSources, trader, traderUserId, historicalDataUserId, p => {}, commission)
+		}
+	}
+	
+}
