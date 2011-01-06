6c7c26878353167d05d044323af5dbdc45c3a29f
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-01-06 19:32:38 +0000

    Issue 67:	Trader optimisation for price slope (regression) variable adopting hill climbing algorithm - DONE

---
+++ trader-examples/src/test/scala/dk/bettingai/trader/hillclimbing/steepestascent/priceslope/SteepestAscentHillClimbingPriceSlopeTraderTest.scala
@@ -0,0 +1,34 @@
+package dk.bettingai.trader.hillclimbing.steepestascent.priceslope
+
+import org.junit._
+import Assert._
+import dk.bettingai.marketsimulator.betex._
+import dk.bettingai.marketsimulator.marketevent._
+import dk.bettingai.marketsimulator._
+import java.io.File
+
+/**Run market simulation for SteepestAscentHillClimbingPriceSlopeTrader in n number of iterations.
+ * 
+ * @author korzekwad
+ *
+ */
+class SteepestAscentHillClimbingPriceSlopeTraderTest {
+
+	val betex = new Betex()
+	val marketEventProcessor = new MarketEventProcessorImpl(betex)
+	val commission = 0.05
+	val simulator = new Simulator(marketEventProcessor,betex,commission)
+
+	val marketDataDir = new File("./src/test/resources/one_hr_10mins_before_inplay")
+	val marketDataSources = Map (marketDataDir.listFiles.filter(_.getName.endsWith(".csv")).map(f => f.getName.split("\\.")(0).toLong -> f) : _*)
+	val trader = new SteepestAscentHillClimbingPriceSlopeTrader()
+
+	@Test def test {
+		
+		for(i <- 1 to 10) {
+			betex.clear()
+			simulator.runSimulation(marketDataSources, trader, p => {})
+		}
+	}
+	
+}
