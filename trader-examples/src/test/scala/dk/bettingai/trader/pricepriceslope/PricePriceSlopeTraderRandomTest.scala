7970084fa57c2f4b24bd519cca1cab0c7fd9a024
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-03-12 00:27:06 +0000

    Issue 97:	Decrease memory usage by market simulator Reuse EPN by multiple trader instances.

--- trader-examples/src/test/scala/dk/bettingai/trader/pricepriceslope/PricePriceSlopeTraderRandomTest.scala
+++ trader-examples/src/test/scala/dk/bettingai/trader/pricepriceslope/PricePriceSlopeTraderRandomTest.scala
@@ -26,6 +26,10 @@ import dk.bettingai.marketsimulator.betex.PriceUtil._
  *  50 markets  01/03/2011 11:19:49 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=829, bestSoFar=Solution [trader=PriceSlopeTrader [id=trader820, backSlope=-0.05, laySlope=-0.03, maxPrice=3.75,mxNumOfRunners=6,minProfitLoss=-62.0,minTradedVolume=405.0], expectedProfit=9.600873255130312, matchedBetsNum=282.0], currentBest=Solution [trader=PriceSlopeTrader [id=trader4144, backSlope=-0.04, laySlope=-0.03, maxPrice=1.46,mxNumOfRunners=11,minProfitLoss=-1.0,minTradedVolume=917.0], expectedProfit=0.062050340423681453, matchedBetsNum=2.0] 
  *  275 markets 03/03/2011 23:24:14 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=100, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader22, backSlope=-0.05, laySlope=-0.04, maxPrice=1.77,mxNumOfRunners=19,minProfitLoss=-91.0,minTradedVolume=178.0], expectedProfit=13.032986027794397, matchedBetsNum=1655.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader500, backSlope=0.03, laySlope=0.0, maxPrice=1.07,mxNumOfRunners=5,minProfitLoss=-31.0,minTradedVolume=673.0], expectedProfit=0.0, matchedBetsNum=0.0] 
  *  50 markets  04/03/2011 08:37:57 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=347, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader1127, backSlope=-0.03, laySlope=-0.03, maxPrice=3.45,mxNumOfRunners=6,minProfitLoss=-79.0,minTradedVolume=167.0], expectedProfit=11.973669978370275, matchedBetsNum=647.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader1736, backSlope=0.04, laySlope=0.02, maxPrice=1.75,mxNumOfRunners=11,minProfitLoss=-60.0,minTradedVolume=548.0], expectedProfit=0.06534685785395755, matchedBetsNum=91.0] 
+ *  50 markets  04/03/2011 15:14:17 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=542, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader230, backSlope=-0.04, laySlope=0.0, maxPrice=5.0,mxNumOfRunners=6,minProfitLoss=-62.0,minTradedVolume=45.0], expectedProfit=14.91159654799338, matchedBetsNum=533.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader2708, backSlope=-0.03, laySlope=-0.01, maxPrice=1.95,mxNumOfRunners=20,minProfitLoss=-34.0,minTradedVolume=594.0], expectedProfit=1.03329440048556, matchedBetsNum=161.0]
+ *  50 markets  04/03/2011 19:17:06 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=766, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader3772, backSlope=0.0, laySlope=0.03, maxPrice=9.2,mxNumOfRunners=9,minProfitLoss=-1369.0,minTradedVolume=1.0], expectedProfit=96.28912720506212, matchedBetsNum=21993.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader3830, backSlope=-0.05, laySlope=0.02, maxPrice=4.8,mxNumOfRunners=3,minProfitLoss=-2158.0,minTradedVolume=667.0], expectedProfit=0.0, matchedBetsNum=0.0] 
+ *  50 markets 05/03/2011 11:47:25 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=993, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader4144, backSlope=-0.04, laySlope=0.0, maxPrice=8.0,mxNumOfRunners=6,minProfitLoss=-1604.0,minTradedVolume=9.0], expectedProfit=119.07696684885481, matchedBetsNum=3069.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader4966, backSlope=0.01, laySlope=0.0, maxPrice=1.04,mxNumOfRunners=7,minProfitLoss=-2653.0,minTradedVolume=488.0], expectedProfit=0.0, matchedBetsNum=0.0] 
+ *  275 markets 07/03/2011 22:42:45 INFO  dk.bettingai.tradingoptimiser.CoevolutionHillClimbing$ - Iter number=144, bestSoFar=Solution [trader=PricePriceSlopeTrader [id=trader353, backSlope=-0.03, laySlope=-0.04, maxPrice=1.81,mxNumOfRunners=17,minProfitLoss=-2232.0,minTradedVolume=22.0], expectedProfit=151.9900618638515, matchedBetsNum=6717.0], currentBest=Solution [trader=PricePriceSlopeTrader [id=trader717, backSlope=0.05, laySlope=0.0, maxPrice=1.24,mxNumOfRunners=17,minProfitLoss=-798.0,minTradedVolume=795.0], expectedProfit=0.0, matchedBetsNum=0.0] 
  *
  */
 class PricePriceSlopeTraderRandomTest {
@@ -47,7 +51,7 @@ class PricePriceSlopeTraderRandomTest {
     var lastTraderId = 1
     def nextTraderId = { lastTraderId += 1; lastTraderId }
 
-  //  val marketDataDir = "c:/daniel/marketdata"
+   // val marketDataDir = "c:/daniel/marketdatafull2"
     val marketDataDir = "./src/test/resources/two_hr_10mins_before_inplay"
 
     /**Full random mutate only.*/
@@ -56,7 +60,7 @@ class PricePriceSlopeTraderRandomTest {
       val layPriceSlopeSignal = ((rand.nextInt(11) - 5) * 0.01)
       val maxPrice = priceUp(1 / rand.nextDouble)
       val maxNumOfRunners = 3 + rand.nextInt(20)
-      val minProfitLoss = rand.nextInt(100) - 100
+      val minProfitLoss = -rand.nextInt(3000)
       val minTradedVolume = rand.nextInt(1000)
       val trader = PricePriceSlopeTrader("trader" + nextTraderId, backPriceSlopeSignal, layPriceSlopeSignal, maxPrice,maxNumOfRunners,minProfitLoss,minTradedVolume)
       trader
