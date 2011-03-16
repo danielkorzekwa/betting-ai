65b9059830f7bff20655716c69fe6b0c5c4e78b1
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-03-16 19:30:36 +0000

    Issue 99:	Optimise traders with coevolution hill climbing gradient with random restarts Refactoring.

--- trading-optimiser/src/main/scala/dk/bettingai/tradingoptimiser/CoevolutionHillClimbing.scala
+++ trading-optimiser/src/main/scala/dk/bettingai/tradingoptimiser/CoevolutionHillClimbing.scala
@@ -77,38 +77,11 @@ object CoevolutionHillClimbing extends ICoevolutionHillClimbing {
      * */
     def optimise(modelTrader: Solution[T], iter: Int): Solution[T] = {
 
-      /**Evaluate fitness for all individuals.
-       * @returns Fitness for the best individual or none if no market data to analyse.
-       */
-      def fitness(population: Seq[T]): Option[Solution[T]] = {
-        /**Create simulation environment.*/
-        val commission = 0.05
-        val simulator = Simulator(commission)
-
-        /**Run simulation and find the best solution.*/
-        val simulationReport = simulator.runSimulation(marketData, population.toList, p => {})
-
-        val bestSolution = simulationReport.marketReports match {
-          case Nil => None
-          case x :: xs => {
-            val traders = simulationReport.marketReports.head.traderReports.map(_.trader)
-            val tradersFitness: List[Tuple2[RegisteredTrader, Double]] = traders.map(t => t -> simulationReport.totalExpectedProfit(t.userId))
-            /** Find best trader [trader, fitness]*/
-            val (bestTrader, expectedProfit) = tradersFitness.reduceLeft((a, b) => if (a._2 > b._2) a else b)
-            val matchedBetsNum = simulationReport.totalMatchedBetsNum(bestTrader.userId)
-
-            Option(Solution(bestTrader.trader.asInstanceOf[T], expectedProfit, matchedBetsNum))
-          }
-        }
-
-        bestSolution
-      }
-
       /** Born 10 traders.*/
       val population = for (i <- 1 to populationSize) yield mutate(modelTrader)
 
       /**Get best of children.*/
-      val bestOfPopulation = fitness(population)
+      val bestOfPopulation = CoevolutionFitness(MarketData(marketData),0.05).fitness(population)
 
       /**Get best of children and parent.*/
       val best: Solution[T] = bestOfPopulation match {
