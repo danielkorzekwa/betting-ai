0c83bc61bdaf9a7e8b73b458878c62e9bcbf286b
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-03-16 18:06:42 +0000

    Issue 99:	Optimise traders with coevolution hill climbing gradient with random restarts Refactoring, moving Solution and PriceTrader classes outside CoevolutionHillClimbing class, so they can be reused by CoevolutionHillClimbingWithRestart.

--- trading-optimiser/src/test/scala/dk/bettingai/tradingoptimiser/CoevolutionHillClimbingTest.scala
+++ trading-optimiser/src/test/scala/dk/bettingai/tradingoptimiser/CoevolutionHillClimbingTest.scala
@@ -8,37 +8,13 @@ import dk.bettingai.marketsimulator.betex.api._
 import IMarket._
 import IBet.BetTypeEnum._
 import CoevolutionHillClimbingTest._
-import ICoevolutionHillClimbing._
 import org.slf4j.LoggerFactory
 import dk.bettingai.marketsimulator.betex.PriceUtil._
 import java.util.Random
 import scala.collection.immutable.TreeMap
 
 object CoevolutionHillClimbingTest {
-  class PriceTrader(val price: Double) extends ITrader {
-
-    val market1Id = 101655622
-    val market1RunnerId = 4207432
-
-    val market2Id = 101655610
-    val market2RunnerId = 3364827
-
-    def execute(ctx: ITraderContext) = {
-
-      if (ctx.marketId == market1Id) {
-        val bestPrices = ctx.getBestPrices(market1RunnerId)
-        if (bestPrices._1.price > price) ctx.fillBet(2, bestPrices._1.price, BACK, market1RunnerId)
-      }
-
-      if (ctx.marketId == market2Id) {
-        val bestPrices = ctx.getBestPrices(market2RunnerId)
-        if (bestPrices._1.price > price) ctx.fillBet(2, bestPrices._1.price, BACK, market2RunnerId)
-      }
-
-    }
-
-    override def toString = "PriceTrader [price=%s]".format(price)
-  }
+ 
 }
 
 class CoevolutionHillClimbingTest {
---
+++ trading-optimiser/src/test/scala/dk/bettingai/tradingoptimiser/PriceTrader.scala
@@ -0,0 +1,30 @@
+package dk.bettingai.tradingoptimiser
+
+import dk.bettingai.marketsimulator.trader._
+import dk.bettingai.marketsimulator.betex.api._
+import IBet.BetTypeEnum._
+
+case class PriceTrader(val price: Double) extends ITrader {
+
+    val market1Id = 101655622
+    val market1RunnerId = 4207432
+
+    val market2Id = 101655610
+    val market2RunnerId = 3364827
+
+    def execute(ctx: ITraderContext) = {
+
+      if (ctx.marketId == market1Id) {
+        val bestPrices = ctx.getBestPrices(market1RunnerId)
+        if (bestPrices._1.price > price) ctx.fillBet(2, bestPrices._1.price, BACK, market1RunnerId)
+      }
+
+      if (ctx.marketId == market2Id) {
+        val bestPrices = ctx.getBestPrices(market2RunnerId)
+        if (bestPrices._1.price > price) ctx.fillBet(2, bestPrices._1.price, BACK, market2RunnerId)
+      }
+
+    }
+
+    override def toString = "PriceTrader [price=%s]".format(price)
+  }
