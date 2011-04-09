956a546912379d0a0d60d01f6331ed9bdca78029
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-04-09 13:55:58 +0000

    Issue 100:	Run trader on a real betting exchange market - Creating UserBets.

---
+++ trunk/live-trader/src/main/scala/dk/bettingai/livetrader/IUserBets.scala
@@ -0,0 +1,32 @@
+package dk.bettingai.livetrader
+
+import dk.bettingai.marketsimulator.betex.api._
+import IBet._
+import BetTypeEnum._
+import BetStatusEnum._
+
+/**
+ * This trait represents an event driven component that provides a quick access to a recent state of user bets. State of user bets is build up from bet events, e.g. betPlaced, betMatched, betCancelled.
+ *
+ * @author korzekwad
+ */
+trait IUserBets {
+
+  /**
+   * Returns matched/unmatched/all user bets for a market id.
+   *
+   * @param marketId
+   * @param betStatus if None (default) all bets are returned.
+   * @return
+   */
+  def getUserBets(marketId: Long, betStatus: Option[BetStatusEnum] = None): List[IBet]
+
+  /**Place unmatched bet.*/
+  def betPlaced(bet: IBet)
+  
+  /**Match bet portion.*/
+  def betMatched(betId:Long,matchedSize: Double)
+
+  /**Cancel a bet.*/
+  def betCancelled(betId:Long)
+}
