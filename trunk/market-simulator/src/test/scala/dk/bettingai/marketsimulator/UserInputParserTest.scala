b31624d3d42b4a6f7917df6eaa24d2b1827bd20f
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-04-19 18:34:51 +0000

    Issue 64:      Trader optimisation for price variable adopting neural networks  - Adding bank parameter (kelly) to SimulatorApp.

--- trunk/market-simulator/src/test/scala/dk/bettingai/marketsimulator/UserInputParserTest.scala
+++ trunk/market-simulator/src/test/scala/dk/bettingai/marketsimulator/UserInputParserTest.scala
@@ -6,61 +6,75 @@ import dk.bettingai.marketsimulator.trader.examples._
 
 class UserInputParserTest {
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testWrongNumberOfInputParameters { UserInputParser.parse(Array("incorrect parameters")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testWrongNumberOfInputParameters { UserInputParser.parse(Array("incorrect parameters")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testWrongMarketDataParameter { UserInputParser.parse(Array("marketDatasss=marketData.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testWrongMarketDataParameter { UserInputParser.parse(Array("marketDatasss=marketData.csv", "traderImpl=dk.bettingai.trader.SimpleTraderImpl")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testWrongTraderImplParameter { UserInputParser.parse(Array("marketData=marketData.csv","traderImpldfdfd=dk.bettingai.trader.SimpleTraderImpl")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testWrongTraderImplParameter { UserInputParser.parse(Array("marketData=marketData.csv", "traderImpldfdfd=dk.bettingai.trader.SimpleTraderImpl")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testWrongFormatOfParameters { UserInputParser.parse(Array("marketDatamarketData.csv","traderImpldfdfddk.bettingai.trader.SimpleTraderImpl")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testWrongFormatOfParameters { UserInputParser.parse(Array("marketDatamarketData.csv", "traderImpldfdfddk.bettingai.trader.SimpleTraderImpl")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testNotExistingMarketDataFile { UserInputParser.parse(Array("marketData=/tmp/blabla/marketdata.csv","traderImpl=dk.bettingai.trader.SimpleTraderImpl")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testNotExistingMarketDataFile { UserInputParser.parse(Array("marketData=/tmp/blabla/marketdata.csv", "traderImpl=dk.bettingai.trader.SimpleTraderImpl")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testTraderImplNotFound { UserInputParser.parse(Array("marketData=src/test/resources/marketDataEmpty.csv","traderImpl=com.dk.bettingai.blabla.SimpleTraderImpl")) }
+  @Test(expected = classOf[IllegalArgumentException])
+  def testTraderImplNotFound { UserInputParser.parse(Array("marketData=src/test/resources/marketDataEmpty.csv", "traderImpl=com.dk.bettingai.blabla.SimpleTraderImpl")) }
 
-	@Test(expected=classOf[IllegalArgumentException])
-	def testTraderImplNoEmptyConstructor { UserInputParser.parse(Array("marketData=src/test/resources/marketDataEmpty.csv","traderImpl=dk.bettingai.marketsimulator.mock.TraderWithoutEmptyConstructor")) }
-	
-	@Test
-	def testTraderImplCorrectDataOneMarketFile { 
-		val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/marketDataPlaceAndCancelLayBet","traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader")) 
-		assertEquals(1,inputData._1.size)
-		assertEquals("10.csv",inputData._1(10).getName)
-		assertTrue(inputData._2.isInstanceOf[NopTrader])
-	}
-	
-	@Test
-	def testTraderImplCorrectDataTwoMarketFiles { 
-		val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/twomarketfiles","traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader")) 
-		assertEquals(2,inputData._1.size)
-		assertEquals("10.csv",inputData._1(10).getName)
-		assertEquals("20.csv",inputData._1(20).getName)
-		assertTrue(inputData._2.isInstanceOf[NopTrader])
-	}
-	
-	@Test def testHtmlReportDirIsNotDefined {
-		val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/twomarketfiles","traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader")) 
-		
-		assertEquals(2,inputData._1.size)
-		assertEquals("10.csv",inputData._1(10).getName)
-		assertEquals("20.csv",inputData._1(20).getName)
-		assertTrue(inputData._2.isInstanceOf[NopTrader])
-		assertEquals("./",inputData._3)
-	}
-	
-	@Test def testHtmlReportDirIsDefined {
-		val inputData = UserInputParser.parse(Array("htmlReportDir=./target","marketDataDir=src/test/resources/twomarketfiles","traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader")) 
-		
-		assertEquals(2,inputData._1.size)
-		assertEquals("10.csv",inputData._1(10).getName)
-		assertEquals("20.csv",inputData._1(20).getName)
-		assertTrue(inputData._2.isInstanceOf[NopTrader])
-		assertEquals("./target",inputData._3)
-	}
+  @Test(expected = classOf[IllegalArgumentException])
+  def testTraderImplNoEmptyConstructor { UserInputParser.parse(Array("marketData=src/test/resources/marketDataEmpty.csv", "traderImpl=dk.bettingai.marketsimulator.mock.TraderWithoutEmptyConstructor")) }
+
+  @Test
+  def testTraderImplCorrectDataOneMarketFile {
+    val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/marketDataPlaceAndCancelLayBet", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+    assertEquals(1, inputData.marketData.size)
+    assertEquals("10.csv", inputData.marketData(10).getName)
+    assertTrue(inputData.trader.isInstanceOf[NopTrader])
+  }
+
+  @Test
+  def testTraderImplCorrectDataTwoMarketFiles {
+    val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/twomarketfiles", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+    assertEquals(2, inputData.marketData.size)
+    assertEquals("10.csv", inputData.marketData(10).getName)
+    assertEquals("20.csv", inputData.marketData(20).getName)
+    assertTrue(inputData.trader.isInstanceOf[NopTrader])
+  }
+
+  @Test
+  def testHtmlReportDirIsNotDefined {
+    val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/twomarketfiles", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+
+    assertEquals(2, inputData.marketData.size)
+    assertEquals("10.csv", inputData.marketData(10).getName)
+    assertEquals("20.csv", inputData.marketData(20).getName)
+    assertTrue(inputData.trader.isInstanceOf[NopTrader])
+    assertEquals("./", inputData.reportDir)
+  }
+
+  @Test
+  def testHtmlReportDirIsDefined {
+    val inputData = UserInputParser.parse(Array("htmlReportDir=./target", "marketDataDir=src/test/resources/twomarketfiles", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+
+    assertEquals(2, inputData.marketData.size)
+    assertEquals("10.csv", inputData.marketData(10).getName)
+    assertEquals("20.csv", inputData.marketData(20).getName)
+    assertTrue(inputData.trader.isInstanceOf[NopTrader])
+    assertEquals("./target", inputData.reportDir)
+  }
+
+  @Test
+  def testBankIsNotDefined {
+    val inputData = UserInputParser.parse(Array("marketDataDir=src/test/resources/twomarketfiles", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+    assertEquals(2500, inputData.bank,0)
+  }
+
+  @Test
+  def testBankIsDefined {
+    val inputData = UserInputParser.parse(Array("bank=1000","marketDataDir=src/test/resources/twomarketfiles", "traderImpl=dk.bettingai.marketsimulator.trader.examples.NopTrader"))
+    assertEquals(1000, inputData.bank,0)
+  }
 }
