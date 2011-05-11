778ca1717f48111b393c14fabfe3ebb3e6d2e54b
daniel.korzekwa <daniel.korzekwa@5635de1c-28a2-11df-8074-0f19218b7f07>
2011-05-11 18:59:47 +0000

    Issue 119:	Create FSM trader

--- trader-examples/src/main/scala/dk/bettingai/trader/fsm/simple/SimpleFsmTrader.scala
+++ trader-examples/src/main/scala/dk/bettingai/trader/fsm/simple/SimpleFsmTrader.scala
@@ -1,18 +1,68 @@
 package dk.bettingai.trader.fsm.simple
 
-import dk.bettingai.marketsimulator.trader.ITrader
+import dk.bettingai.trader.fsm.simple.SimpleFsmTrader._
+import dk.bettingai.trader._
+import scala.collection._
 import dk.bettingai.marketsimulator.trader._
-
-/**Creates a number of state machines, each of them trying to place a bet and then trade it with a profit or a minimal loss. 
+import dk.bettingai.marketsimulator.betex.api._
+import dk.bettingai.marketsimulator.betex._
+import IBet.BetTypeEnum._
+import IBet.BetStatusEnum._
+import IMarket._
+/**
+ * Creates a number of state machines, each of them trying to place a bet and then trade it with a profit or a minimal loss.
  * This is a proof of concept for fsm only.
- * 
+ *
  * @author korzekwad
- * 
+ *
  */
+case class RunnerTrader(val runnerId: Long) extends ITrader {
+
+  sealed abstract class TraderState
+  case object Init extends TraderState
+  case class BackPlaced(backBetId: Long) extends TraderState
+  case class LayPlaced(layBetId: Long) extends TraderState
+  var state: TraderState = Init
+
+  val betSize = 2d
+  def execute(ctx: ITraderContext) {
+
+    val bestPrices = ctx.getBestPrices(runnerId)
+    val matchedBets = ctx.getBets(true)
+
+    if (!bestPrices._1.price.isNaN && !bestPrices._2.price.isNaN)
+      state match {
+        case Init => {
+          val bet = ctx.placeBet(betSize, bestPrices._2.price, BACK, runnerId)
+          state = BackPlaced(bet.betId)
+        }
+        case BackPlaced(backBetId) => {
+          if (matchedBets.exists(b => b.betId == backBetId)) {
+            val bet = ctx.placeBet(betSize, bestPrices._1.price, LAY, runnerId)
+            state = LayPlaced(bet.betId)
+          }
+        }
+        case LayPlaced(layBetId) => {
+          if (matchedBets.exists(b => b.betId == layBetId)) {
+            state = Init
+          }
+        }
+      }
+  } //end of execute
+
+}
+
 class SimpleFsmTrader extends ITrader {
 
-	def execute(ctx:ITraderContext) {
-		println(ctx.marketId)
-	}
-	
+  /**key - runnerId.*/
+  var backTraders = mutable.ListBuffer[ITrader]()
+  override def init(ctx: ITraderContext) {
+    for (i <- 1 to 1) ctx.runners.foreach(r => backTraders += RunnerTrader(r.runnerId))
+  }
+
+  def execute(ctx: ITraderContext) {
+    backTraders.foreach(t => t.execute(ctx))
+
+  }
+
 }
